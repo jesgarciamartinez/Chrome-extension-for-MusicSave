4847ce593679c365d2acf63e2d3531b2403c2d6d
Jesús García <jgarmar2@gmail.com>
2015-08-25 04:10:26 +0200

    use strict works

--- content.js
+++ content.js
@@ -1,3 +1,4 @@
+'use strict'
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
     if (request.message === "clicked_browser_action") {
         console.log('content has received message');
