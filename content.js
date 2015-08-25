0162b562b4922d05dbbce9a5a1c07f6b179520ee
Jesús García <jgarmar2@gmail.com>
2015-08-25 03:49:34 +0200

    first commit, extension works

---
+++ content.js
@@ -0,0 +1,11 @@
+
+
+
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+    if (request.message === "clicked_browser_action") {
+        console.log('content has received message');
+        var $title = $('#eow-title').prop('title');
+    }
+    chrome.runtime.sendMessage({"message": "open_new_tab", "artistTitle": $title});
+  }
+);
---
+++ popup.js
@@ -0,0 +1,68 @@
+//'use strict'
+var trackToPost = {};
+
+document.addEventListener('DOMContentLoaded', function() {
+  //chrome.pageAction.setIcon(icon_128_active.png);
+  // var artistTitle = getYoutubeArtistAndTitle();
+  // var artist = artistTitle[0];
+  // var title = artistTitle[1];
+
+  // getCurrentTabUrl(function(url){
+  //   $('h1').text(artist + title);
+  //   objectPost = {artist: artist, title: title, url: url};
+  //   console.log(objectPost);
+  //   $('button').attr('disabled', false);
+      chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+      var activeTab = tabs[0];
+      trackToPost.url = activeTab.url;
+      chrome.tabs.sendMessage(activeTab.id, {"message": "clicked_browser_action"});
+    });
+});
+
+chrome.runtime.onMessage.addListener(
+  function(request, sender, sendResponse) {
+    if( request.message === "open_new_tab" ) {
+      var $title = request.artistTitle;
+      var artist;
+      var title;
+      if ($title.indexOf('-') !== -1){
+        artist = $title.slice(0, $title.indexOf('-')).trim();
+        title = $title.slice($title.indexOf('-') + 1).trim();
+      }
+      else{
+        artist = '';
+        title = $title.trim();
+      }
+      trackToPost.artist = artist;
+      trackToPost.title = title;
+      $('button').attr('disabled', false);
+      $('h1').text(artist + title + trackToPost.url);
+      $('button').on('click', function(event){
+    // var $currentTarget = event.currentTarget;
+        var url = 'http://localhost:3000/tracks';
+        $.post(url, {track: trackToPost});
+      });
+    }
+  }
+);
+
+// $('button').on('click', function(event){
+//     // var $currentTarget = event.currentTarget;
+//     var url = 'http://localhost:3000/tracks';
+//     $.post(url, {track: trackToPost});
+// });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
